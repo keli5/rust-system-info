8600c94a550cbee8d2166765ae1b5d5a61327164
letters <letters@localhost.localdomain>
2020-11-24 01:20:14 -0600

    Add CPU stuff

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@ use sysinfo::*;
 fn main() {
     let mut system = sysinfo::System::new_all();
     system.refresh_all();
-    
+    println!("CPU\n---\n");
+    println!("{} cores", system.get_processors().len());
+
+    for cpu in system.get_processors() {
+        let cpunum = cpu.get_name().replace("cpu", "");
+        let mut usage = cpu.get_cpu_usage() * 100.;
+        usage = usage.round() / 100.;
+        println!("Core #{}: {}% usage", cpunum, usage)
+    }
+
+    println!("\nDisks\n---\n");
 
     for disk in system.get_disks() {
         let totalsize = disk.get_total_space() as i64;
         let freespace = disk.get_available_space() as i64;
+
         println!("Disk: {:?}", disk.get_name());
         println!("--- Type: {:?}", disk.get_type());
         println!("--- Mountpoint: {:?}", disk.get_mount_point());
