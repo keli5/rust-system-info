340f15de617e920e424aa2bd5f9480f25cf4871e
letters <letters@localhost.localdomain>
2020-11-24 08:56:48 -0600

    prompt cpu/disk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use sysinfo::*;
+use text_io::read;
 
 fn main() {
     let mut system = sysinfo::System::new_all();
     system.refresh_all();
-    println!("CPU\n---\n");
+    println!("Info to show [CPU/disk]:");
+    let mut line: String = read!("{}\n");
+
+    line = line.to_lowercase();
+
+    if line == "" {
+        line = String::from("cpu");
+    }
+
+    if line == "cpu" {
+        cpuinfo(system)
+    } else if line == "disk" {
+        diskinfo(system)
+    } else {
+        println!("Invalid choice recieved.")
+    }
+}
+
+fn cpuinfo(system: sysinfo::System) {
     println!("{} cores", system.get_processors().len());
 
     for cpu in system.get_processors() {
@@ -12,12 +31,13 @@ fn main() {
         usage = usage.round() / 100.;
         println!("Core #{}: {}% usage", cpunum, usage)
     }
+}
 
-    println!("\nDisks\n---\n");
-
+fn diskinfo(system: sysinfo::System) {
     for disk in system.get_disks() {
         let totalsize = disk.get_total_space() as i64;
         let freespace = disk.get_available_space() as i64;
+        // TODO: disk.get_name() returns "" for physical disks on windows
 
         println!("Disk: {:?}", disk.get_name());
         println!("--- Type: {:?}", disk.get_type());
@@ -26,6 +46,4 @@ fn main() {
         println!("--- Free space: {} GB", freespace / 1e+9 as i64 );
         print!("\n")
     }
-    println!("Done.")
 }
-
